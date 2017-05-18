503f981c9ec4c50c6c42ff9f223bf9341eb75bf2
Obi Uchenna David <daviduchenna@outlook.com>
2017-05-18 10:33:56 +0100

    added Profile Admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user__first_name', 'address', 'phone_number']
+    search_fields = ['phone_number', 'address']
--- app/models.py
+++ app/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
