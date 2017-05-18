b919b66919ecadb2273ede6ff595befe1116cf9c
Obi Uchenna David <daviduchenna@outlook.com>
2017-05-18 10:36:16 +0100

    create Super user

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.site.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user__first_name', 'address', 'phone_number']
+    list_display = ['user', 'address', 'phone_number']
     search_fields = ['phone_number', 'address']
+
+
+admin.site.register(Profile, ProfileAdmin)
