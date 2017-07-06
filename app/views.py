b3c7b93df2b721a9731de44b9e926d46048deded
Obi Uchenna David <daviduchenna@outlook.com>
2017-07-06 23:04:24 +0100

    registeration

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
+from .models import Profile
+
+from django.contrib.auth.models import User
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -16,8 +19,25 @@ def register(request):
         email = request.POST.get("email")
         address = request.POST.get("address")
         number = request.POST.get("number")
+        password = request.POST.get("password")
+        cpassword = request.POST.get("cpassword")
 
+        u = User()
 
+        u.first_name = fname
+        u.last_name = lname
+        u.username = uname
+        u.email = email
 
+        u.set_password(password)
+        u.save()
+
+        p = Profile()
+        p.user = u
+        p.address = address
+        p.phone_number = number
+        p.save()
+
+        return redirect('app:index')
 
     return render(request, 'app/register.djt')
